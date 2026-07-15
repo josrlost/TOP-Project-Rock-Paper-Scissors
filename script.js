7e199b03237cf554fdd385b1145ed97c8a5c152c
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-15 03:37:47 +0000

    Code corrected. Replaced conditional statements within the function with a strict inequality of the respective value, succesfully confirming that the function outputs one of the three values everytime the console is reloaded

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function getComputerChoice() {
     let paper = 'Paper';
     let rock = 'Rock';
     let randomNumber = Math.floor(Math.random() * 3) + 1;
-    if(randomNumber > 1) {
+    if(randomNumber === 1) {
         return console.log(scissors);
-    } else if(randomNumber > 2) {
+    } else if(randomNumber === 2) {
         return console.log(paper);
     } else {
         return console.log(rock);
