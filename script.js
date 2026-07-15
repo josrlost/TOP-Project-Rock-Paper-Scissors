7452f383be4d6288c64f713290de4028bbbb2353
josrlost <197001265+josrlost@users.noreply.github.com.>
2026-07-15 03:33:43 +0000

    Initially declared the requested function, as well as defining the initial local variables for the function. Utilized Math floor and math random accordingly, but after calling the function on the console it is noted that the function in question only returns a random value from 1 to 2

---
+++ script.js
@@ -0,0 +1,18 @@
+//console.log('Hello World');
+
+function getComputerChoice() {
+    let scissors = 'Scissors';
+    let paper = 'Paper';
+    let rock = 'Rock';
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    if(randomNumber > 1) {
+        return console.log(scissors);
+    } else if(randomNumber > 2) {
+        return console.log(paper);
+    } else {
+        return console.log(rock);
+    }
+}
+
+getComputerChoice();
+
